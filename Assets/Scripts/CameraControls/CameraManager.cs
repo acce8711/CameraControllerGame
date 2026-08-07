428507466793cd21f788b65084819b21b933bbfc
acce8711 <101413675+acce8711@users.noreply.github.com>
2026-08-07 18:19:27 -0400

    prototype picture taking

---
+++ Assets/Scripts/CameraControls/CameraManager.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CameraManager : MonoBehaviour
+{
+    public GameObject picture_camera;
+    public RenderTexture picture_texture;
+    public RawImage picture_display;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void TakePicture()
+    {
+        picture_camera.SetActive(true);
+        Camera camera = picture_camera.GetComponent<Camera>();
+        camera.targetTexture = picture_texture;
+        camera.Render();
+        camera.targetTexture = null;
+        picture_camera.SetActive(false);
+
+        picture_display.texture = picture_texture;
+    }
+}
